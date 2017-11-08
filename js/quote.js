04c2cd60bfd6ff2ecee8c2cd24dda4b294c56b35
Geoffrey ROYER <geoffrey.royer@gmail.com>
2017-11-08 19:28:21 +0100

    Add quotes.

--- js/quote.js
+++ js/quote.js
@@ -3,6 +3,15 @@ var quotes = [
   'Katsuya Miyahira, parce que rira bien qui miyahira le dernier.',
   'Le stage termine à 21h15. Bon des fois ça déborde sur 21h16 voire 17.',
   'Faîtes des squats, comme les immeubles inhabités.',
+  'Il vous reste dix minutes, ou deux fois cinq minutes. Faîtes votre choix,' +
+  ' dix ou deux fois cinq !',
+  'Nan mais regarde comme il est raide, c\'est un PLAY-MO-BIL !',
+  'Quel est le mot du bruit qu\'on fait quand on a trop mangé et qu\'on a ' +
+  'des gaz ? Le mot ? ... Roté, oui. Moroté !',
+  'G., quand tu fais tes impulsions faut qu\'elles ressemblent à des ' +
+  'kïais, ici c\'est pas opération séduction "eiiihhh... mmmh...", hein.',
+  'Alors quand on fait un Kïai on est pas chez le dentiste. Vous êtes pas ' +
+  'obligé de montrer vos dents... <i>H-Eyhh..</i>!'
 ]
 
 window.onload = function() {
